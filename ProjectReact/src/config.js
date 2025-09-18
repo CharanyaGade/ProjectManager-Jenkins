7f8e62d68f9fae9cb3ad030608da5d09d33117ae
Charanya Gade <charanyagade@gmail.com>
2025-09-18 09:51:24 +0530

    Updated Config.js File

--- ProjectReact/src/config.js
+++ ProjectReact/src/config.js
@@ -1,11 +1,5 @@
 const config = {
-  DEV_API_URL: "http://localhost:1982/projectapi",
-  TOMCAT_API_URL: "http://localhost:2030/springbootprojectapi/projectapi"
-};
+  url :"http://localhost:2030/springbootprojectapi"
+}
 
-const API_URL =
-  window.location.hostname === "localhost" && window.location.port === "5173"
-    ? config.DEV_API_URL
-    : config.TOMCAT_API_URL;
-
-export default API_URL;
+export default config;
